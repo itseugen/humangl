3d8e75e7adbc9478e70d71359ff41f2f1aa8fc68
Eugen <eugen.weiberle@gmail.com>
2026-04-22 15:45:06 +0200

    Sum cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@ int main()
 	glEnable(GL_DEPTH_TEST);
 
 	float last = (float)glfwGetTime();
-
+	float angle = 0.0f;
 	while(!glfwWindowShouldClose(app._win))
 	{
 		float now = (float)glfwGetTime();
@@ -27,11 +27,30 @@ int main()
 		// Cloar buffer
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		Mat4 MVP = app.calcMVP(app._body.torso);
 
-		// app.drawCube(MVP, Colour(1.0f, 0.0f, 0.0f), TextureType::None);
-		app.drawCube(MVP, app._body.torso.colour, app._body.torso.tex);
+		// Moves the body on the x axis (shows attachment)
+		// static float posX = 0.0f;
+		// posX -= 0.5f * app._dt;
+		// app._body.torso.base = translate(posX, 0.0f, 0.0f);
 
+		// if (last >= 5.0f)
+		// {
+		// 	app._body.torso.shape = mat4_scale(Vec3{10.0f, 10.0f, 10.0f});
+		// }
+		angle += 4.0f * app._dt;
+		if (angle >= 360.0f) angle -= 360.0f;
+
+		static float posX = 0.0f;
+		posX -= 0.5f * app._dt;
+		app._body.torso.base = translate(posX, 0.0f, 0.0f) * translate(0.0f, 0.0f, 0.0f) * rotate_y(angle * (3.14159265f / 180.0f));
+		Mat4 torsoMVP = app.calcMVP(app._body.torso);
+		app.drawCube(torsoMVP, app._body.torso.colour, app._body.torso.tex);
+
+		float torsoHalf = app._body.torso.shape.m[5] * 0.5f;
+		float headHalf  = app._body.head.shape.m[5]  * 0.5f;
+		app._body.head.base = app._body.torso.base * translate(0.0f, torsoHalf + headHalf, 0.0f) * nod(now, 2.0f);
+		Mat4 headMVP = app.calcMVP(app._body.head);
+		app.drawCube(headMVP, app._body.head.colour, app._body.head.tex);
 		// Display rendered stuff
 		glfwSwapBuffers(app._win);
 		// Mouse and keyboard events
